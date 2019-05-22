c0325af3faff35132b5757f54eb993ded67c104c
张云 <1015649494@qq.com>
2019-05-22 16:27:35 +0800

    05-22

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 
 import state from './state';
-// import mutations from './mutations';
+import mutations from './mutations';
 // import actions from './actions';
 // import getters from './getters';
 
@@ -11,7 +11,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state,
-    // mutations,
+    mutations,
     // actions,
     // getters
 })
